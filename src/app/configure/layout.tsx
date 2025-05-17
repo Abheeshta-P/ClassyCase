a9687035bda8a07062563f9cc5bdfe1925574cc9
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-05-17 23:13:34 +0530

    add steps

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from '@/components/layouts/MaxWidthWrapper'
+import Steps from '@/components/molecules/Steps';
 import { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className='flex-1 flex flex-col'>
+      <Steps/>
       {children}
     </MaxWidthWrapper>
   )
--- src/app/constants/const.ts
+++ src/app/constants/const.ts
@@ -5,4 +5,22 @@ export const PHONES = [
   "/testimonials/4.jpg",
   "/testimonials/5.jpg",
   "/testimonials/6.jpg",
-]
+];
+
+export const STEPS = [
+  {
+    name: "Step 1: Add image",
+    description: "Choose an image for your case",
+    url: "/upload",
+  },
+  {
+    name: "Step 2: Customize design",
+    description: "Make the case yours",
+    url: "/design",
+  },
+  {
+    name: "Step 3: Summary",
+    description: "Review your final design",
+    url: "/preview",
+  },
+];
