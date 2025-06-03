e8581780d3bae47f7dfe3bca7361b30733795cfa
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-06-03 19:06:09 +0530

    add color options

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -6,6 +6,10 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import NextImage from "next/image";
 import { Rnd } from "react-rnd";
+import { Radio, RadioGroup } from '@headlessui/react';
+import { useState } from "react";
+import { COLOR } from "@/validators/option-validator";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string
@@ -14,38 +18,42 @@ interface DesignConfiguratorProps {
 }
 
 function DesignConfigurator({ configId, imgURL, imageDimensions }: DesignConfiguratorProps) {
+  const [options, setOptions] = useState<{
+    color: (typeof COLOR)[number]
+  }>({
+    color: COLOR[1],
+  })
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
-          <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full" >
-            <NextImage fill alt="phone template" src={"/images/phone-template.png"} className="pointer-events-none" />
-          </AspectRatio>
-          
-          {/* Hover then change the background */}
-          <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]">
-            <div className={cn("absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]", ``)}/>
-          </div>
-
+            <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
+              <NextImage fill alt="phone template" src={"/images/phone-template.png"} className="pointer-events-none z-50 select-none" />
+            </AspectRatio>
+            
+            {/* Hover then change the background */}
+            <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+            <div className={cn("absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]", `bg-${options.color.tw}`)}/>
         </div>
         <Rnd default={{
-          x: 150,
-          y: 205,
-          height: imageDimensions.height / 4,
-          width: imageDimensions.width / 4
-        }}
-          lockAspectRatio
-          resizeHandleComponent={{
-            bottomRight: <HandleComponent/>,
-            bottomLeft: <HandleComponent/>,
-            topRight: <HandleComponent/>,
-            topLeft: <HandleComponent/>,
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4
           }}
-          className="absolute z-20 ring-[2px] ring-primary"
-        >
-          <div className="relative w-full h-full">
-            <NextImage className="pointer-events-none" src={imgURL} alt="your image" fill/>
-          </div>
+            lockAspectRatio
+            resizeHandleComponent={{
+              bottomRight: <HandleComponent/>,
+              bottomLeft: <HandleComponent/>,
+              topRight: <HandleComponent/>,
+              topLeft: <HandleComponent/>,
+            }}
+            className="absolute z-20 ring-[2px] ring-primary"
+          >
+            <div className="relative w-full h-full">
+              <NextImage className="pointer-events-none select-none" src={imgURL} alt="your image" fill/>
+            </div>
         </Rnd>
       </div>
       <div className="h-[37.5rem] flex flex-col bg-white">
@@ -63,6 +71,42 @@ function DesignConfigurator({ configId, imgURL, imageDimensions }: DesignConfigu
             <div className='w-full h-px bg-zinc-200 my-6' />
 
             {/* options */}
+            <div className="relative mt-4 h-full flex flex-col sm:flex-row justify-between">
+              <RadioGroup value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: val,
+                  }))
+                }}
+              >
+                <Label>Color: {options.color.label}</Label>
+                <div className='mt-3 flex items-center space-x-3'>
+                  {COLOR.map((color) => (
+                    <div key={color.label}>
+                      <Radio
+                        value={color}
+                        className={({ checked }) =>
+                          cn(
+                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent',
+                            {
+                              [`border-${color.tw}`]: checked,
+                            }
+                          )
+                        }
+                      >
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            'h-8 w-8 rounded-full border border-black border-opacity-10'
+                          )}
+                        />
+                      </Radio>
+                    </div>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
           </div>
         </ScrollArea>
       </div>
---
+++ src/validators/option-validator.ts
@@ -0,0 +1,11 @@
+// bg-zinc-900 border-zinc-900
+// bg-zinc-50 border-zinc-50
+// bg-blue-950 border-blue-950
+// bg-rose-950 border-rose-950
+
+export const COLOR = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "White", value: "white", tw: "zinc-50" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+] as const;
