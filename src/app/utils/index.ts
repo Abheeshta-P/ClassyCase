a09abbc069796c3e2f7889786dbc4ce9553c0d50
Abheeshta-P <softwarecheck01+github@gmail.com>
2025-06-21 20:58:17 +0530

    add dashboard for admin

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -11,3 +11,18 @@ export function splitArray<T>(array: Array<T>, numParts: number) {
 
   return result;
 }
+
+export function getDaysInMonth():number {
+  const now = new Date();
+  const month = now.getMonth();
+  const year = now.getFullYear();
+  // Months with 31 days
+  const thirtyOneDays = [0, 2, 4, 6, 7, 9, 11];
+
+  if (thirtyOneDays.includes(month)) return 31;
+  if (month === 1) {
+    // February check for leap year
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 29 : 28;
+  }
+  return 30;
+}
